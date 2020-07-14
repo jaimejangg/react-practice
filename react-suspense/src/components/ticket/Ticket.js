5f36ea3614799e9f7ea74ed09c4e6390f8b92e63
Jaime Rueda <jaime_rueda10@hotmail.com>
2020-07-14 15:57:30 -0500

    react hoc suspense

---
+++ react-suspense/src/components/ticket/Ticket.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const TicketWrapper = styled.div`
+    background: darkGray;
+    padding: 20px;
+    border-radius: 20px;
+    
+    &:not(:last-child) {
+        margin-bottom: 5%;
+        margin-right: ${props => !!props.marginRight ? '1%' : '0'};
+    }
+`;
+
+const Title = styled.h3`
+    width: 100%;
+    margin: 0px;
+`;
+
+const Body = styled.p`
+    width: 100%;
+`;
+
+const Ticket = ({ marginRight, ticket, onDragStart }) => (
+    <TicketWrapper marginRight={marginRight} draggable onDragStart={e => onDragStart(e, ticket.id)}>
+        <Title>{ticket.title}</Title>
+        <Body>{ticket.body}</Body>
+    </TicketWrapper>
+);
+
+export default Ticket;
---
+++ react-suspense/src/containers/Board.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import styled from 'styled-components';
+import withDataFetching from "../withDataFetching";
+import Lane from '../components/Lane/Lane';
+
+const BoardWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  margin: 5%;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+ 
+class Board extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      tickets: [],
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.data !== this.props.data) {
+      this.setState({ tickets: this.props.data })
+    }
+  };
+
+  onDragStart = (e, id) => {
+    e.dataTransfer.setData('id', id)
+  };
+
+  onDragOver = e => {
+    e.preventDefault();
+  };
+
+  onDrop = (e, laneId) => {
+    const id = e.dataTransfer.getData('id');
+
+    const tickets = this.state.tickets.filter(ticket => {
+      if (ticket.id === id) {
+        ticket.board = laneId;
+      }
+      return ticket;
+    })
+
+    this.setState({
+      ...this.state,
+      tickets
+    });
+  };
+
+  render() {
+    const { lanes, loading, error } = this.props;
+
+    return (
+      <BoardWrapper>
+        {lanes.map(lane =>
+          <Lane
+            key={lane.id}
+            laneId={lane.id}
+            title={lane.title}
+            loading={loading}
+            error={error}
+            onDragStart={this.onDragStart}
+            onDragOver={this.onDragOver}
+            onDrop={this.onDrop}
+            tickets={this.state.tickets.filter(ticket => ticket.lane === lane.id)}
+          />
+        )}
+      </BoardWrapper>
+    );
+  }
+}
+
+export default withDataFetching(Board);
